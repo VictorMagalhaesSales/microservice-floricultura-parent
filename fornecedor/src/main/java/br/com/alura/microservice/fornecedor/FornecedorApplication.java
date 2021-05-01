1540dee563c703aaecdea174c6e3ed119de32cb8
Victor Magalhães <victormagalhaessales@gmail.com>
2021-05-01 17:35:03 -0300

    Criando resource server em loja e fornecedor

--- fornecedor/src/main/java/br/com/alura/microservice/fornecedor/FornecedorApplication.java
+++ fornecedor/src/main/java/br/com/alura/microservice/fornecedor/FornecedorApplication.java
@@ -2,8 +2,10 @@ package br.com.alura.microservice.fornecedor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class FornecedorApplication {
 
 	public static void main(String[] args) {
